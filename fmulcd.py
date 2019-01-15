a88330858d2c6dce8767bf7c68365d334c1a530f
jonathan roth <jwayneroth@gmail.com>
2019-01-15 13:03:56 -0500

    basic window running

--- fmulcd.py
+++ fmulcd.py
@@ -9,15 +9,17 @@ import subprocess
 import logging
 from mpd_client import *
 import fmuglobals
-import buttons
-import pygameui as ui
+#import buttons
+#import pygameui as ui
+import Tkinter as tk
+import ttk as ttk
 
-from albumlist import *
-from controls import *
+#from albumlist import *
+#from controls import *
 from nowplaying import *
-from radio import *
-from settings import *
-from screensaver import *
+#from radio import *
+#from settings import *
+#from screensaver import *
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
@@ -34,224 +36,268 @@ logger.addHandler(ch)
 logger.addHandler(fh)
 
 class FMU(object):
-    def __init__(self):
-        self.current = False
-        self.last = False
-        self.screen_dimensions = (320,480)
-        self.screen = False
-        self.ss_timer = 0
-        self.ss_timer_on = True
-        self.ss_delay = 60000
+	def __init__(self):
+		self.current = False
+		self.last = False
+		self.screen_dimensions = (320,480)
+		self.screen = False
+		self.ss_timer = 0
+		self.ss_timer_on = True
+		self.ss_delay = 60000
+		
+		self.root = tk.Tk()
+		self.container = tk.Frame(self.root, width=320, height=480, borderwidth=0).grid()
+		self.make_sidebar()
+		self.root.title('FMU Player')
+		
+		if fmuglobals.RUN_ON_RASPBERRY_PI:
+			os.environ['SDL_FBDEV'] = '/dev/fb1'
+			os.environ["SDL_NOMOUSE"] = "1"
+			os.environ['SDL_MOUSEDEV'] = '/dev/input/touchscreen'
+			os.environ['SDL_MOUSEDRV'] = 'TSLIB'
 
-        if fmuglobals.RUN_ON_RASPBERRY_PI:
-            os.environ['SDL_FBDEV'] = '/dev/fb1'
-            os.environ["SDL_NOMOUSE"] = "1"
-            os.environ['SDL_MOUSEDEV'] = '/dev/input/touchscreen'
-            os.environ['SDL_MOUSEDRV'] = 'TSLIB'
+		#self.init_pygame()
 
-        self.init_pygame()
+		#ui.theme.init()
+		#ui.theme.use_theme(ui.theme.dark_theme)
 
-        ui.theme.init()
-        ui.theme.use_theme(ui.theme.dark_theme)
+		#rect = pygame.Rect((0,0),self.screen_dimensions)
 
-        rect = pygame.Rect((0,0),self.screen_dimensions)
+		self.scenes = {
+			'NowPlaying': NowPlayingScene(rect),
+			#'Albums': AlbumListScene(rect),
+			#'Radio': RadioScene(rect),
+			#'Settings': SettingsScene(rect),
+			#'Controls': ControlsScene(rect),
+			#'Screensaver': ScreensaverScene(rect)
+		}
 
-        self.scenes = {
-            'NowPlaying': NowPlayingScene(rect),
-            'Albums': AlbumListScene(rect),
-            'Radio': RadioScene(rect),
-            'Settings': SettingsScene(rect),
-            'Controls': ControlsScene(rect),
-            'Screensaver': ScreensaverScene(rect)
-        }
+		for name,scene in self.scenes.iteritems():
+			scene.on_nav_change.connect(self.change_scene)
 
-        for name,scene in self.scenes.iteritems():
-            scene.on_nav_change.connect(self.change_scene)
+		print 'created scenes'
 
-        print 'created scenes'
+		self.make_current_scene(self.scenes['NowPlaying'])
 
-        self.make_current_scene(self.scenes['NowPlaying'])
+		#self.ab = buttons.AnalogButtons()
+	
+	def make_sidebar(self):
+		
+		sidebar = tk.Frame(self.container, borderwidth=0)
+		
+		btns = [
+			('NowPlaying','cd'),
+			('Albums','list'),
+			('Radio','music'),
+			('Settings','cog'),
+			('Controls','volume-down')
+		]
+		
+		btn_col = 0;
+		
+		for btn_data in btns:
+			btn_img = tk.PhotoImage(file='images/icon.gif', width=20, height=20)# + btn_data[1])
+			btn = tk.Button(sidebar, image=btn_img, width=50, height=50, borderwidth=0)
+			btn.image = btn_img
+			def cb(evt, self=self, btn_data=btn_data):
+				return self.sidebar_btn_clicked(evt, btn_data[0])
+			btn.bind('<Button-1>', cb)
+			btn.grid(row=0, column=btn_col)
+			btn_col = btn_col + 1
+		
+		sidebar.grid(row=0, column=0, sticky=tk.N)
+		
+	def sidebar_btn_clicked(self, evt, btn):
+		"""
+		btn.state = 'normal'
+		self.main_active = True
+		self.on_main_active()
+		self.on_nav_change(btn.tag_name)
+		"""
+		logger.debug('sidebar_btn_clicked: %s' % btn)
+		
+	"""
+	init_pygame
+	"""
+	def init_pygame(self):
+		# this section is an unbelievable nasty hack - for some reason Pygame
+		# needs a keyboardinterrupt to initialise in some limited circs (second time running)
+		class Alarm(Exception):
+			pass
 
-        self.ab = buttons.AnalogButtons()
+		def alarm_handler(signum, frame):
+			raise Alarm
 
-    """
-    init_pygame
-    """
-    def init_pygame(self):
-        # this section is an unbelievable nasty hack - for some reason Pygame
-        # needs a keyboardinterrupt to initialise in some limited circs (second time running)
-        class Alarm(Exception):
-            pass
+		signal(SIGALRM, alarm_handler)
 
-        def alarm_handler(signum, frame):
-            raise Alarm
+		alarm(3)
 
-        signal(SIGALRM, alarm_handler)
+		try:
+			pygame.init()
 
-        alarm(3)
+			if fmuglobals.RUN_ON_RASPBERRY_PI:
+				pygame.mouse.set_visible(False)
+				display_flags = pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.ANYFORMAT
+				self.screen = pygame.display.set_mode( (self.screen_dimensions), display_flags )
+			else:
+				self.screen = pygame.display.set_mode(self.screen_dimensions)
 
-        try:
-            pygame.init()
+			alarm(0)
 
-            if fmuglobals.RUN_ON_RASPBERRY_PI:
-                pygame.mouse.set_visible(False)
-                display_flags = pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.ANYFORMAT
-                self.screen = pygame.display.set_mode( (self.screen_dimensions), display_flags )
-            else:
-                self.screen = pygame.display.set_mode(self.screen_dimensions)
+		except Alarm:
+			raise KeyboardInterrupt
 
-            alarm(0)
+		logger.debug('pygame inited on rpi?: %s' % fmuglobals.RUN_ON_RASPBERRY_PI)
 
-        except Alarm:
-            raise KeyboardInterrupt
+		pygame.key.set_repeat(300,180)
+		pygame.display.set_caption('Raspberry Pi UI')
+		#pygame.event.set_allowed(None)
+		#pygame.event.set_allowed(( pygame.QUIT, pygame.KEYDOWN ))
 
-        logger.debug('pygame inited on rpi?: %s' % fmuglobals.RUN_ON_RASPBERRY_PI)
-
-        pygame.key.set_repeat(300,180)
-        pygame.display.set_caption('Raspberry Pi UI')
-        #pygame.event.set_allowed(None)
-        #pygame.event.set_allowed(( pygame.QUIT, pygame.KEYDOWN ))
-
-    """
-    signal_handler
-    """
-    def signal_handler(self, signal, frame):
-        print '\nFMULCD::signal_handler: {}'.format(signal)
-        time.sleep(1)
-        pygame.display.quit()
-        fmu.kill_app()
+	"""
+	signal_handler
+	"""
+	def signal_handler(self, signal, frame):
+		print '\nFMULCD::signal_handler: {}'.format(signal)
+		time.sleep(1)
+		pygame.display.quit()
+		fmu.kill_app()
 
 
-    """
-    make_current_scene
-    """
-    def make_current_scene(self, scene):
-        print 'FMULCD::make_current_scene \t' + scene.name
-        #if self.current == scene:
-        #    return
-        if self.current:
-            self.current.exited()
-            self.last = self.current
-        self.current = scene
-        self.current.entered()
-        self.current.refresh()
+	"""
+	make_current_scene
+	"""
+	def make_current_scene(self, scene):
+		print 'FMULCD::make_current_scene \t' + scene.name
+		#if self.current == scene:
+		#	 return
+		if self.current:
+			self.current.exited()
+			self.last = self.current
+		self.current = scene
+		self.current.entered()
+		self.current.refresh()
 
-    """
-    change_scene
-     called from a PiScene on_nav_change
-     push requested scene to ui and refresh it
-    """
-    def change_scene(self, scene_name, refresh=False, from_screensaver=False):
-        if from_screensaver:
-            self.ss_timer_on = True
-            if self.last:
-                self.make_current_scene(self.last)
-                return
-        if refresh == True:
-            self.scenes['Albums'].populate_artists_view()
-        self.make_current_scene(self.scenes[scene_name])
+	"""
+	change_scene
+	 called from a PiScene on_nav_change
+	 push requested scene to ui and refresh it
+	"""
+	def change_scene(self, scene_name, refresh=False, from_screensaver=False):
+		if from_screensaver:
+			self.ss_timer_on = True
+			if self.last:
+				self.make_current_scene(self.last)
+				return
+		if refresh == True:
+			self.scenes['Albums'].populate_artists_view()
+		self.make_current_scene(self.scenes[scene_name])
 
-    def screensaver_tick(self, ms, activity):
-        if activity:
-            self.ss_timer = 0
-        else:
-            self.ss_timer += ms
-            if self.ss_timer >= self.ss_delay:
-                logger.debug('going to screensaver')
-                self.ss_timer_on = False
-                self.ss_timer = 0
-                self.change_scene('Screensaver')
+	def screensaver_tick(self, ms, activity):
+		if activity:
+			self.ss_timer = 0
+		else:
+			self.ss_timer += ms
+			if self.ss_timer >= self.ss_delay:
+				logger.debug('going to screensaver')
+				self.ss_timer_on = False
+				self.ss_timer = 0
+				self.change_scene('Screensaver')
 
-    def kill_app(self):
-        #if fmuglobals.RUN_ON_RASPBERRY_PI:
-        #    GPIO.output(18, GPIO.LOW)
-        pygame.quit()
-        sys.exit(0)
+	def kill_app(self):
+		#if fmuglobals.RUN_ON_RASPBERRY_PI:
+		#	 GPIO.output(18, GPIO.LOW)
+		pygame.quit()
+		sys.exit(0)
 
 
 """
 main
 """
 if __name__ == '__main__':
-    logger.debug('fmulcd started')
-    fmu = FMU()
-    clock = pygame.time.Clock()
-    fps = 12 if fmuglobals.RUN_ON_RASPBERRY_PI else 30
-    ticks = 0
+	logger.debug('fmulcd started')
+	fmu = FMU()
+	#clock = pygame.time.Clock()
+	#fps = 12 if fmuglobals.RUN_ON_RASPBERRY_PI else 30
+	#ticks = 0
+	
+	fmu.root.mainloop()
+	
+	"""
+	while True:
+		ticks = clock.tick(fps)
 
-    while True:
-        ticks = clock.tick(fps)
+		down_in_view = None
+		user_active = False
 
-        down_in_view = None
-        user_active = False
+		for e in pygame.event.get():
 
-        for e in pygame.event.get():
+			if e.type == pygame.QUIT:
+				fmu.kill_app()
+				break
 
-            if e.type == pygame.QUIT:
-                fmu.kill_app()
-                break
+			mousepoint = pygame.mouse.get_pos()
 
-            mousepoint = pygame.mouse.get_pos()
+			if e.type == pygame.KEYDOWN:
+				user_active = True
+				if (( e.key == pygame.K_ESCAPE )):
+					fmu.kill_app()
+				else:
+					fmu.current.key_down(e.key, e.unicode)
+					break
 
-            if e.type == pygame.KEYDOWN:
-                user_active = True
-                if (( e.key == pygame.K_ESCAPE )):
-                    fmu.kill_app()
-                else:
-                    fmu.current.key_down(e.key, e.unicode)
-                    break
+			elif e.type == pygame.MOUSEBUTTONDOWN:
+				user_active = True
 
-            elif e.type == pygame.MOUSEBUTTONDOWN:
-                user_active = True
+				hit_view = fmu.current.hit(mousepoint)
 
-                hit_view = fmu.current.hit(mousepoint)
+				#logger.debug('hit %s at %s' % (hit_view, mousepoint))
 
-                #logger.debug('hit %s at %s' % (hit_view, mousepoint))
+				if (hit_view is not None and
+					not isinstance(hit_view, ui.Scene)
+				):
+					ui.focus.set(hit_view)
+					down_in_view = hit_view
+					pt = hit_view.from_window(mousepoint)
+					hit_view.mouse_down(e.button, pt)
+				else:
+					ui.focus.set(None)
 
-                if (hit_view is not None and
-                    not isinstance(hit_view, ui.Scene)
-                ):
-                    ui.focus.set(hit_view)
-                    down_in_view = hit_view
-                    pt = hit_view.from_window(mousepoint)
-                    hit_view.mouse_down(e.button, pt)
-                else:
-                    ui.focus.set(None)
+			elif e.type == pygame.MOUSEBUTTONUP:
+				user_active = True
+				hit_view = fmu.current.hit(mousepoint)
+				logger.debug('click %s at %s' % (hit_view, mousepoint))
 
-            elif e.type == pygame.MOUSEBUTTONUP:
-                user_active = True
-                hit_view = fmu.current.hit(mousepoint)
-                logger.debug('click %s at %s' % (hit_view, mousepoint))
+				if fmu.current == fmu.scenes['Screensaver']:
+					fmu.change_scene('NowPlaying', False, True)
+					break
 
-                if fmu.current == fmu.scenes['Screensaver']:
-                    fmu.change_scene('NowPlaying', False, True)
-                    break
+				if hit_view is not None:
+					if down_in_view and hit_view != down_in_view:
+						down_in_view.blurred()
+						ui.focus.set(None)
+					pt = hit_view.from_window(mousepoint)
+					hit_view.mouse_up(e.button, pt)
+				down_in_view = None
 
-                if hit_view is not None:
-                    if down_in_view and hit_view != down_in_view:
-                        down_in_view.blurred()
-                        ui.focus.set(None)
-                    pt = hit_view.from_window(mousepoint)
-                    hit_view.mouse_up(e.button, pt)
-                down_in_view = None
+			elif e.type == pygame.MOUSEMOTION:
+				user_active = True
+				if down_in_view and down_in_view.draggable:
+					pt = down_in_view.from_window(mousepoint)
+					down_in_view.mouse_drag(pt, e.rel)
+				else:
+					fmu.current.mouse_motion(mousepoint)
 
-            elif e.type == pygame.MOUSEMOTION:
-                user_active = True
-                if down_in_view and down_in_view.draggable:
-                    pt = down_in_view.from_window(mousepoint)
-                    down_in_view.mouse_drag(pt, e.rel)
-                else:
-                    fmu.current.mouse_motion(mousepoint)
+		if fmu.ss_timer_on:
+			fmu.screensaver_tick(ticks, user_active)
 
-        if fmu.ss_timer_on:
-            fmu.screensaver_tick(ticks, user_active)
+		fmu.current.update()
 
-        fmu.current.update()
+		if fmu.current.draw():
 
-        if fmu.current.draw():
+			fmu.screen.blit(fmu.current.surface, (0, 0))
 
-            fmu.screen.blit(fmu.current.surface, (0, 0))
+			pygame.display.flip()
 
-            pygame.display.flip()
-
-        #time.sleep(.05)
+		#time.sleep(.05)
+		"""
--- piscene.py
+++ piscene.py
@@ -1,263 +1,228 @@
 from mpd_client import *
-import pygameui as ui
+#import pygameui as ui
 
 """
 PiScene
  pygameui.Scene subclass
  parent class for all fmulcd scenes
 """
-class PiScene(ui.Scene):
-    def __init__(self, frame=None, name='PiScene'):
+class PiScene():
+	def __init__(self, frame=None, name='PiScene'):
 
-        ui.Scene.__init__(self, frame)
+		#ui.Scene.__init__(self, frame)
 
-        self.name = name
-        self.margins = 15
-        self.btn_size = 45
-        self.margins_bottom = 10
-        self.has_nav = False
-        self.is_mpd_listener = False
-        self.label_height = 45
-        self.controls_on = False
-        self.modal_left_right_margin = 0
-        self.modal_top_bottom_margin = 0
-        self.music_directory = '/var/lib/mpd/music/'
-        self.main_active = False
-        self.sidebar_index = 0
-        self.active_sidebar_btn = 0
+		self.name = name
+		self.margins = 15
+		self.btn_size = 45
+		self.margins_bottom = 10
+		self.has_nav = False
+		self.is_mpd_listener = False
+		self.label_height = 45
+		self.controls_on = False
+		self.modal_left_right_margin = 0
+		self.modal_top_bottom_margin = 0
+		self.music_directory = '/var/lib/mpd/music/'
+		self.main_active = False
+		self.sidebar_index = 0
+		self.active_sidebar_btn = 0
 
-        self.sidebar = self.make_sidebar()
-        self.main = self.make_main()
-        #self.controls = self.make_controls()
+		#self.sidebar = self.make_sidebar()
+		self.main = self.make_main()
+		#self.controls = self.make_controls()
 
-        self.add_child(self.sidebar)
-        self.add_child(self.main)
+		self.add_child(self.sidebar)
+		self.add_child(self.main)
 
-        self.on_nav_change = callback.Signal()
+		self.on_nav_change = callback.Signal()
 
-    """
-    make_sidebar
-    """
-    def make_sidebar(self):
+	"""
+	make_sidebar
+	"""
+	"""
+	def make_sidebar(self):
 
-        self.sidebar_btns = []
+		self.sidebar_btns = []
 
-        sidebar = ui.View(
-            ui.Rect(
-                self.margins,
-                self.margins,
-                self.frame.width,
-                self.btn_size
-            )
-        )
+		sidebar = ui.View(
+			ui.Rect(
+				self.margins,
+				self.margins,
+				self.frame.width,
+				self.btn_size
+			)
+		)
 
-        btns = [
-            ('NowPlaying','cd'),
-            ('Albums','list'),
-            ('Radio','music'),
-            ('Settings','cog'),
-            ('Controls','volume-down')
-        ]
+		btns = [
+			('NowPlaying','cd'),
+			('Albums','list'),
+			('Radio','music'),
+			('Settings','cog'),
+			('Controls','volume-down')
+		]
 
-        btn_x = 0
+		btn_x = 0
 
-        for btn_data in btns:
-            btn = ui.IconButton(
-                ui.Rect(
-                    btn_x,0,
-                    self.btn_size,
-                    self.btn_size
-                ),
-                btn_data[1]
-            )
-            btn_x = btn_x + self.btn_size + self.margins
-            btn.on_clicked.connect(self.sidebar_btn_clicked)
-            btn.tag_name = btn_data[0]
+		for btn_data in btns:
+			btn = ui.IconButton(
+				ui.Rect(
+					btn_x,0,
+					self.btn_size,
+					self.btn_size
+				),
+				btn_data[1]
+			)
+			btn_x = btn_x + self.btn_size + self.margins
+			btn.on_clicked.connect(self.sidebar_btn_clicked)
+			btn.tag_name = btn_data[0]
 
-            if btn_data[0] == self.name:
-                btn.state = 'selected'
-            sidebar.add_child(btn)
-            self.sidebar_btns.append(btn)
+			if btn_data[0] == self.name:
+				btn.state = 'selected'
+			sidebar.add_child(btn)
+			self.sidebar_btns.append(btn)
 
-        return sidebar
+		return sidebar
+	"""
+	
+	"""
+	make_main
+	"""
+	def make_main(self):
 
-    """
-    make_main
-    """
-    def make_main(self):
+		main = tk.Frame()
+		return main
 
-        main = PiMain( #ui.View(
-            ui.Rect(
-                0,
-                self.sidebar.frame.bottom,
-                self.frame.width,
-                self.frame.height - self.sidebar.frame.height # - self.margins - self.margins
-            )
-        )
+	"""
+	key_down
+	"""
+	def key_down(self, key, code):
+		#print 'key_down key: ' + str(key) + ' code: ' + str(code)
 
-        return main
+		#ui.Scene.key_down(self,key,code)
 
-    """
-    make_controls
-    """
-    def make_controls(self):
+		if self.main_active == True:
+			self.key_down_main(key)
+		else:
+			self.key_down_sidebar(key)
 
-        controls = PiControls(ui.Rect(
-            self.modal_left_right_margin,
-            self.modal_top_bottom_margin,
-            self.frame.width - self.modal_left_right_margin * 2,
-            self.frame.height - self.modal_top_bottom_margin * 2
-        ))
+	"""
+	key_down_main
+	"""
+	def key_down_main(self, key):
+		if key == pygame.K_LEFT:
+			self.main_active = False
+			self.active_sidebar_btn = 0
+			self.sidebar_btns[self.active_sidebar_btn].state = 'focused'
 
-        controls.on_dismissed.connect(self.onControlsDismissed)
+	"""
+	key_down_sidebar
+	"""
+	def key_down_sidebar(self, key):
+		if key == pygame.K_LEFT:
+			if self.active_sidebar_btn > 0:
+				if self.sidebar_btns[self.active_sidebar_btn].tag_name == self.name:
+					self.sidebar_btns[self.active_sidebar_btn].state = 'selected'
+				else:
+					self.sidebar_btns[self.active_sidebar_btn].state = 'normal'
+				self.active_sidebar_btn = self.active_sidebar_btn - 1
+				self.sidebar_btns[self.active_sidebar_btn].state = 'focused'
 
-        return controls
+		elif key == pygame.K_RIGHT:
+			if self.active_sidebar_btn < (len(self.sidebar_btns) - 1):
+				if self.sidebar_btns[self.active_sidebar_btn].tag_name == self.name:
+					self.sidebar_btns[self.active_sidebar_btn].state = 'selected'
+				else:
+					self.sidebar_btns[self.active_sidebar_btn].state = 'normal'
+				self.active_sidebar_btn = self.active_sidebar_btn + 1
+				self.sidebar_btns[self.active_sidebar_btn].state = 'focused'
 
-    """
-    onControlsDismissed
-    """
-    def onControlsDismissed(self):
-        self.controls_on = False
+		elif key == pygame.K_UP:
+			pass
 
-    """
-    sidebar_btn_clicked
-    """
-    def sidebar_btn_clicked(self, btn, mouse_btn):
-        btn.state = 'normal'
-        self.main_active = True
-        self.on_main_active()
-        self.on_nav_change(btn.tag_name)
+		elif key == pygame.K_DOWN:
+			self.main_active = True
+			if self.sidebar_btns[self.active_sidebar_btn].tag_name == self.name:
+				self.sidebar_btns[self.active_sidebar_btn].state = 'selected'
+			else:
+				self.sidebar_btns[self.active_sidebar_btn].state = 'normal'
+			self.on_main_active()
 
-    """
-    key_down
-    """
-    def key_down(self, key, code):
-        #print 'key_down key: ' + str(key) + ' code: ' + str(code)
+		elif key == pygame.K_RETURN:
+			self.sidebar_btn_clicked(self.sidebar_btns[self.active_sidebar_btn], False)
 
-        ui.Scene.key_down(self,key,code)
+	"""
+	on_main_active
+	"""
+	def on_main_active(self):
+		pass
 
-        if self.main_active == True:
-            self.key_down_main(key)
-        else:
-            self.key_down_sidebar(key)
+	"""
+	refresh
+	"""
+	def refresh(self):
+		self.active_sidebar_btn = self.sidebar_index
+		self.main_active = True
+		self.on_main_active()
 
-    """
-    key_down_main
-    """
-    def key_down_main(self, key):
-        if key == pygame.K_LEFT:
-            self.main_active = False
-            self.active_sidebar_btn = 0
-            self.sidebar_btns[self.active_sidebar_btn].state = 'focused'
+	"""
+	entered
+	"""
+	def entered(self):
+		ui.Scene.entered(self)
 
-    """
-    key_down_sidebar
-    """
-    def key_down_sidebar(self, key):
-        if key == pygame.K_LEFT:
-            if self.active_sidebar_btn > 0:
-                if self.sidebar_btns[self.active_sidebar_btn].tag_name == self.name:
-                    self.sidebar_btns[self.active_sidebar_btn].state = 'selected'
-                else:
-                    self.sidebar_btns[self.active_sidebar_btn].state = 'normal'
-                self.active_sidebar_btn = self.active_sidebar_btn - 1
-                self.sidebar_btns[self.active_sidebar_btn].state = 'focused'
+	"""
+	update
+	"""
+	def update(self):
+		#ui.Scene.update(self, dt)
+		if self.is_mpd_listener == True:
+			if mpd.status_get():
+				self.on_mpd_update()
 
-        elif key == pygame.K_RIGHT:
-            if self.active_sidebar_btn < (len(self.sidebar_btns) - 1):
-                if self.sidebar_btns[self.active_sidebar_btn].tag_name == self.name:
-                    self.sidebar_btns[self.active_sidebar_btn].state = 'selected'
-                else:
-                    self.sidebar_btns[self.active_sidebar_btn].state = 'normal'
-                self.active_sidebar_btn = self.active_sidebar_btn + 1
-                self.sidebar_btns[self.active_sidebar_btn].state = 'focused'
+	"""
+	on_mpd_update
+	"""
+	def on_mpd_update(self):
+		pass
 
-        elif key == pygame.K_UP:
-            pass
+class PiMain():
+	def __init__(self, frame=None):
+		pass
+		
+	"""
+	draw
+	if we have been updated or forced, force children to redraw and redraw ourselves
+	else only redraw if any children have been redrawn themselves
+	"""
+	def draw(self, force=False):
+		if self.hidden:
+			return False
+		
+		if self.updated or force:
+			self.updated = False
+			
+			for child in self.children:
+				if not child.hidden:
+					child.draw(True)
+					self.surface.blit(child.surface, child.frame.topleft)
+					if child.border_color and child.border_widths is not None:
+						if (type(child.border_widths) is int and child.border_widths > 0):
+							pass
+							#pygame.draw.rect(self.surface, child.border_color, child.frame, child.border_widths)
+			return True
 
-        elif key == pygame.K_DOWN:
-            self.main_active = True
-            if self.sidebar_btns[self.active_sidebar_btn].tag_name == self.name:
-                self.sidebar_btns[self.active_sidebar_btn].state = 'selected'
-            else:
-                self.sidebar_btns[self.active_sidebar_btn].state = 'normal'
-            self.on_main_active()
+		else:
+			drawn = False
 
-        elif key == pygame.K_RETURN:
-            self.sidebar_btn_clicked(self.sidebar_btns[self.active_sidebar_btn], False)
+			for child in self.children:
+				if not child.hidden:
+					if child.draw():
+						#if child.__class__.__name__ != 'View':
+						#print 'Pi Main redrawing ' + child.__class__.__name__
+						drawn = True
+						self.surface.blit(child.surface, child.frame.topleft)
+						if child.border_color and child.border_widths is not None:
+							if (type(child.border_widths) is int and child.border_widths > 0):
+								pass
+								#pygame.draw.rect(self.surface, child.border_color, child.frame, child.border_widths)
 
-    """
-    on_main_active
-    """
-    def on_main_active(self):
-        pass
-
-    """
-    refresh
-    """
-    def refresh(self):
-        self.active_sidebar_btn = self.sidebar_index
-        self.main_active = True
-        self.on_main_active()
-
-    """
-    entered
-    """
-    def entered(self):
-        ui.Scene.entered(self)
-
-    """
-    update
-    """
-    def update(self):
-        #ui.Scene.update(self, dt)
-        if self.is_mpd_listener == True:
-            if mpd.status_get():
-                self.on_mpd_update()
-
-    """
-    on_mpd_update
-    """
-    def on_mpd_update(self):
-        pass
-
-class PiMain(ui.View):
-    def __init__(self, frame=None):
-        ui.View.__init__(self, frame)
-
-    """
-    draw
-     if we have been updated or forced, force children to redraw and redraw ourselves
-     else only redraw if any children have been redrawn themselves
-    """
-    def draw(self, force=False):
-        if self.hidden:
-            return False
-
-        if self.updated or force:
-            self.updated = False
-
-            for child in self.children:
-                if not child.hidden:
-                    child.draw(True)
-                    self.surface.blit(child.surface, child.frame.topleft)
-                    if child.border_color and child.border_widths is not None:
-                        if (type(child.border_widths) is int and child.border_widths > 0):
-                            pygame.draw.rect(self.surface, child.border_color, child.frame, child.border_widths)
-            return True
-
-        else:
-            drawn = False
-
-            for child in self.children:
-                if not child.hidden:
-                    if child.draw():
-                        #if child.__class__.__name__ != 'View':
-                        #print 'Pi Main redrawing ' + child.__class__.__name__
-                        drawn = True
-                        self.surface.blit(child.surface, child.frame.topleft)
-                        if child.border_color and child.border_widths is not None:
-                            if (type(child.border_widths) is int and child.border_widths > 0):
-                                pygame.draw.rect(self.surface, child.border_color, child.frame, child.border_widths)
-
-            return drawn
+			return drawn
