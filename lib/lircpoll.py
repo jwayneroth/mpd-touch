39649ceaad2e35e5044888a00d8d3f0d889f26ff
Jonathan Roth <jwayneroth@gmail.com>
2019-04-12 13:13:34 -0400

    fix lircpoll to handle retrigger of same key

--- lib/lircpoll.py
+++ lib/lircpoll.py
@@ -26,63 +26,71 @@ class Irw:
 		self.sock.connect(SOCKPATH)
 		self.last_poll = (None, '00')
 		self.last_key = (None, '00')
-		
+
 	def _run(self):
 		self._running = True
 		while self._running:
 			self.poll()
-		
+
 	def run(self):
 		if self._thread is not None:
 			return
-		
+
 		self._thread = threading.Thread(target=self._run)
-		
+
 		self._thread.start()
-	
+
 	def last(self):
 		lkk, lkr = self.last_key
 		lpk, lpr = self.last_poll
-		
+
+		#print 'poll: %s %s' % (lpk, lpr)
+		#print 'last: %s %s' % (lkk, lkr)
+
 		# TODO: consecutive clicks will not trigger (lpr will be less than or equal to lpkr)
 		# if no change since previous call, empty vars
 		if lkk == lpk and lkr == lpr:
 			self.last_key = (None, '00')
 			self.last_poll = (None, '00')
-		
-		# if last key has been repeated only once, ignore
-		elif lkk == lpk and (int(lpr, 16) - int(lkr, 16) <= self.throttle):
-			return None
+
+		# if key is same, but repeat is different
+		elif lkk == lpk:
+			#if new repeat is more than last but under throttle, ignore
+			if lpr > lkr and (int(lpr, 16) - int(lkr, 16) <= self.throttle):
+				return None
+			self.last_key = self.last_poll
+
+		# key is different, we have new trigger
 		else:
 			self.last_key = self.last_poll
 		return self.last_key[0]
-	
+
 	def next_key(self):
 		while True:
 			data = self.sock.recv(128)
 			data = data.strip()
 			if data:
 				break
-		
+
 		words = data.split()
 		return words[2], words[1]
-	
+
 	def poll(self):
 		keyname, updown = self.next_key()
 		self.last_poll = (keyname, updown)
-		
+
 if __name__ == "__main__":
 	import signal
 
-	irw = Irw(3)
-	
+	irw = Irw(8)
+
 	irw.run()
-	
+
 	while 1:
 		try:
 			last_key = irw.last()
-			if last_key is not None:
-				print 'last key is %s ' % last_key
-			time.sleep(1)
+			#if last_key is not None:
+			print 'last key is %s ' % last_key
+			time.sleep(.3)
 		except KeyboardInterrupt:
 			exit()
