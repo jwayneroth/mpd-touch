7127ffef819c08c7c32166d5b7ef95090e32e434
Jonathan Roth <jwayneroth@gmail.com>
2024-10-22 13:49:27 -0400

    add timeout to controls dialog

--- web/client/public/js/app.js
+++ web/client/public/js/app.js
@@ -695,25 +695,31 @@ function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input ==
 /**
  * Controls Modal
  */
+
+var MODAL_TIMEOUT = 60000;
 var ControlsDialog = /*#__PURE__*/function () {
   function ControlsDialog(el) {
     _classCallCheck(this, ControlsDialog);
     console.log('ControlsDialog::init');
     var i;
     var controls = el.querySelectorAll('.btn-group .btn[data-tag-name]');
+    this.modalShown = false;
+    this.modalTimeoutID = null;
     this.dom = {
       el: el,
       navButton: document.querySelector('#main-nav a[data-bs-target="#controls"]'),
       playPause: el.querySelector("button[data-tag-name='play_pause']"),
       playMode: el.querySelector("button[data-tag-name='play_mode']"),
       volumeDisplay: el.querySelector('#controls__volume-display'),
-      volumeSlider: el.querySelector('#controls__volume')
+      volumeSlider: el.querySelector('#controls__volume'),
+      close: el.querySelector('#controls__close')
     };
     for (i = 0; i < controls.length; i++) {
       controls[i].addEventListener('click', this.controlClick.bind(this));
     }
     this.dom.volumeSlider.addEventListener('change', this.onVolumeChange.bind(this));
     el.addEventListener('shown.bs.modal', this.onModalShown.bind(this));
+    el.addEventListener('hidden.bs.modal', this.onModalHidden.bind(this));
     window.addEventListener('mpdstatus', this.onMpdStatus.bind(this));
   }
   _createClass(ControlsDialog, [{
@@ -740,14 +746,15 @@ var ControlsDialog = /*#__PURE__*/function () {
         this.setVolume(0);
         this.onVolumeChange();
       } else if (tag_name == 'volume-down') {
-        this.setVolume(parseInt(this.dom.volumeSlider.value, 10) - 10);
+        this.setVolume(parseInt(this.dom.volumeSlider.value, 10) - 5);
         this.onVolumeChange();
       } else if (tag_name == 'volume-up') {
-        this.setVolume(parseInt(this.dom.volumeSlider.value, 10) + 10);
+        this.setVolume(parseInt(this.dom.volumeSlider.value, 10) + 5);
         this.onVolumeChange();
       } else {
         _api__WEBPACK_IMPORTED_MODULE_0__.axios.get(_api__WEBPACK_IMPORTED_MODULE_0__.API_URL + '/controls/' + tag_name).then(this.updateControlButtons.bind(this));
       }
+      this.setModalTimeout();
       return false;
     }
   }, {
@@ -779,6 +786,8 @@ var ControlsDialog = /*#__PURE__*/function () {
       // 	console.log(error);
       // })
       ;
+
+      this.setModalTimeout();
     }
   }, {
     key: "setVolume",
@@ -796,6 +805,36 @@ var ControlsDialog = /*#__PURE__*/function () {
         _this2.setVolume(parseInt(response.data.volume, 10));
         _this2.dom.volumeSlider.removeAttribute('disabled');
       });
+      this.modalShown = true;
+      this.setModalTimeout();
+    }
+  }, {
+    key: "onModalHidden",
+    value: function onModalHidden() {
+      console.log('onModalHidden');
+      this.modalShown = false;
+      if (this.modalTimeoutID) {
+        window.clearTimeout(this.modalTimeoutID);
+        this.modalTimeoutID = null;
+      }
+    }
+  }, {
+    key: "setModalTimeout",
+    value: function setModalTimeout() {
+      if (!this.modalShown) return;
+      if (this.modalTimeoutID) {
+        window.clearTimeout(this.modalTimeoutID);
+        this.modalTimeoutID = null;
+      }
+      this.modalTimeoutID = window.setTimeout(this.timeoutFire.bind(this), MODAL_TIMEOUT);
+    }
+  }, {
+    key: "timeoutFire",
+    value: function timeoutFire() {
+      console.log('timeoutFire');
+      this.modalTimeoutID = null;
+      // click close
+      this.dom.close.click();
     }
   }]);
   return ControlsDialog;
--- web/client/src/js/controls-dialog.js
+++ web/client/src/js/controls-dialog.js
@@ -3,6 +3,9 @@ import { axios, API_URL } from './api';
 /**
  * Controls Modal
  */
+
+const MODAL_TIMEOUT = 60000;
+
 export default class ControlsDialog {
 	constructor(el) {
 		console.log('ControlsDialog::init');
@@ -11,6 +14,9 @@ export default class ControlsDialog {
 
 		const controls = el.querySelectorAll('.btn-group .btn[data-tag-name]');
 
+		this.modalShown = false;
+		this.modalTimeoutID = null;
+
 		this.dom = {
 			el,
 			navButton: document.querySelector('#main-nav a[data-bs-target="#controls"]'),
@@ -18,6 +24,7 @@ export default class ControlsDialog {
 			playMode: el.querySelector("button[data-tag-name='play_mode']"),
 			volumeDisplay: el.querySelector('#controls__volume-display'),
 			volumeSlider: el.querySelector('#controls__volume'),
+			close: el.querySelector('#controls__close'),
 		};
 
 		for (i = 0; i < controls.length; i++) {
@@ -27,6 +34,7 @@ export default class ControlsDialog {
 		this.dom.volumeSlider.addEventListener('change', this.onVolumeChange.bind(this));
 
 		el.addEventListener('shown.bs.modal', this.onModalShown.bind(this));
+		el.addEventListener('hidden.bs.modal', this.onModalHidden.bind(this));
 
 		window.addEventListener('mpdstatus', this.onMpdStatus.bind(this));
 	}
@@ -56,15 +64,17 @@ export default class ControlsDialog {
 			this.setVolume(0);
 			this.onVolumeChange();
 		} else if (tag_name == 'volume-down') {
-			this.setVolume(parseInt(this.dom.volumeSlider.value, 10) - 10);
+			this.setVolume(parseInt(this.dom.volumeSlider.value, 10) - 5);
 			this.onVolumeChange();
 		} else if (tag_name == 'volume-up') {
-			this.setVolume(parseInt(this.dom.volumeSlider.value, 10) + 10);
+			this.setVolume(parseInt(this.dom.volumeSlider.value, 10) + 5);
 			this.onVolumeChange();
 		} else {
 			axios.get(API_URL + '/controls/' + tag_name)
 				.then(this.updateControlButtons.bind(this))
 		}
+
+		this.setModalTimeout();
 
 		return false;
 	}
@@ -95,6 +105,8 @@ export default class ControlsDialog {
 			// 	console.log(error);
 			// })
 			;
+
+		this.setModalTimeout();
 	}
 
 	setVolume(vol) {
@@ -111,5 +123,40 @@ export default class ControlsDialog {
 				this.setVolume(parseInt(response.data.volume, 10));
 				this.dom.volumeSlider.removeAttribute('disabled');
 			})
+
+		this.modalShown = true;
+
+		this.setModalTimeout();
+	}
+
+	onModalHidden() {
+		console.log('onModalHidden');
+
+		this.modalShown = false;
+
+		if (this.modalTimeoutID) {
+			window.clearTimeout(this.modalTimeoutID);
+			this.modalTimeoutID = null;
+		}
+	}
+
+	setModalTimeout() {
+
+		if (!this.modalShown) return;
+
+		if (this.modalTimeoutID) {
+			window.clearTimeout(this.modalTimeoutID);
+			this.modalTimeoutID = null;
+		}
+
+		this.modalTimeoutID = window.setTimeout(this.timeoutFire.bind(this), MODAL_TIMEOUT);
+	}
+
+	timeoutFire() {
+		console.log('timeoutFire');
+
+		this.modalTimeoutID = null;
+		// click close
+		this.dom.close.click();
 	}
 }
